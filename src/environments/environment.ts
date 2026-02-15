2a3a3a82b25003b5102d4dd5416a315e9bb57885
Walison Fernandes <walison.alves@datainfo.inf.br>
2026-02-15 00:27:43 -0300

    feat(core): instalando supabase clieant and environment config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-
-};
+  supabaseUrl: 'https://xstazhllcrpfxvjlkxib.supabase.co',
+  supabaseKey: 'sb_publishable_EIxEAE5-UwHd4dNtAQqr6Q_Ocv0Ac3L'};
